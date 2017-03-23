89b7111dffff86b2f8e8326f38595fa0929508b0
90300588 <90300588@hs-03236-macair.ad.edenpr.org>
2017-03-23 13:15:15 -0500

    I'm committed to normandale

--- src/TwoDArrayBasics.java
+++ src/TwoDArrayBasics.java
@@ -1,19 +1,40 @@
+import java.util.Random;
 
 public class TwoDArrayBasics {
 
 	public static void main(String[] args) {
 		// Create a 3 x 5 array of ints
-		
+		int[][] nums = new int [3][5];
 		// Fill the array with random numbers using fillRandom
-		
+		fillRandom(nums,1,100);
 		// test each method you create below.
-
+printArray(nums);
+int rowTotal = sumRow(nums,0);
+System.out.println(rowTotal);
 	}
 	/*
 	 * given the lower and upper bounds of the random numbers
 	 * fill an array of random numbers 
 	 */
+	public static void fillRandom(int[][]nums, int low, int high){
+		Random random = new Random();
+		for (int r =0; r<nums.length; r++){
+			for(int c=0; c<nums[0].length; c++){
+				nums[r][c]= random.nextInt(high)+low;
+			}
+		}
+	}
 	
+	public static void printArray(int[][] nums){
+		for (int r =0; r<nums.length; r++){
+			System.out.print("{");
+			for(int c=0; c<nums[0].length; c++){
+				System.out.print(nums[r][c] + "," );
+			}
+				System.out.println("}");
+			}
+		
+	}
 	/*
 	 * create a method that prints a 2D array
 	 */
@@ -29,7 +50,12 @@ public class TwoDArrayBasics {
 	 * return an array of row sums of a 2D array (nums)
 	 */
 	
-
+public static int sumRow(int[][]nums,int row){
+	int total = 0; 
+	for(int c = 0; c<nums[0].length; c++)
+		total += nums[row][c];
+	return total;
+}
 	
 	
 	/*
